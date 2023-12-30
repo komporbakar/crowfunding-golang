dfe08276111df8d78635837d025ae5d65933a865
komporbakar <laravelgg00@gmail.com>
2023-12-30 12:10:16 +0700

    add exp jwt token

--- auth/service.go
+++ auth/service.go
@@ -1,6 +1,10 @@
 package auth
 
-import "github.com/golang-jwt/jwt/v5"
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
 
 type Service interface {
 	GenerateToken(userId int) (string, error)
@@ -18,6 +22,7 @@ var SECRET_KEY = []byte("RAHAS1A")
 func (s *jwtService) GenerateToken(userId int) (string, error) {
 	payload := jwt.MapClaims{}
 	payload["user_id"] = userId
+	payload["exp"] = time.Now().Add(time.Hour * 12).Unix()
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
 
